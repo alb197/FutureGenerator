586452b74b2b8fe045579ad88a4764de959e1c70
Alina bengert <alb197@pitt.edu>
2015-07-24 20:02:00 -0400

    added the game plan

---
+++ src/FutureGen.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Alina Bengert on 7/24/2015.
+ */
+public class FutureGen {
+
+}
---
+++ src/FutureGenTest.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Alina Bengert on 7/24/2015.
+ */
+
+import org.junit.*;
+
+public class FutureGenTest {
+    //Testing item was added to ____ property
+    @Test
+    public void test1()
+    {
+
+    }
+
+    //Testing item was added to ____ property
+    @Test
+    public void test2()
+    {
+
+    }
+}
