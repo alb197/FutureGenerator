91d763a1188f1c068547e36958c0a001280f70d8
Alina bengert <alb197@pitt.edu>
2015-07-28 21:08:11 -0400

    tested numbergenerator and addCategories methods, everything passed, created methods :)

--- src/FutureGen.java
+++ src/FutureGen.java
@@ -1,6 +1,7 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by Alina Bengert on 7/24/2015.
@@ -16,24 +17,42 @@ public class FutureGen {
     private static ArrayList<String> numberOfChildren = new ArrayList<String> ();
     private static ArrayList<String> typeOfResidence = new ArrayList<String> ();
     private static ArrayList<String> typeOfPet = new ArrayList<String> ();
+    private static final ArrayList<ArrayList<String>> categoriesArray = new ArrayList<ArrayList<String>>();
     private static String person;
     private static String place;
     private static String children;
     private static String residence;
     private static String pet;
     private static String car;
+    private static final String [] categories = new String []{"Person you will marry (ex: Chris, Channing etc.)", " Country you will live in (ex: Russia, Japan etc.)", "Type of car you will drive (ex: Subaru, Limo etc.)"," Pet you will have (ex: Dog, Tiger etc.)", "Residence you will live in (ex: House, Mansion etc.)", "Number of Children you will pop out (ex: 2, 11 etc.)" };
+    private static final String PositiveMessage = "Please enter %s that you do like.";
+    private static final String NegativeMessage = "Please enter %s that you do not like.";
     public static void main (String [] args){
         try {
             FutureGen futureGen = new FutureGen();
             System.out.println(displayFutureGenMessage(new ByteArrayOutputStream(), "Welcome to the Future Generator"));
             System.out.println(displayFutureGenMessage(new ByteArrayOutputStream(), "For each category choose two options you want in your future and one you don't. What will your future hold?"));
-        } catch (IOException e) {
+            addCategories();
+           //populateArrayList();
+            for (int i = 0; i < categories.length; i++) {
+                for (int j = 0; j <= 2; j++) {
+                    if (j == 0 || j == 1) {
+                        System.out.println(displayFutureGenMessage(new ByteArrayOutputStream(), String.format(PositiveMessage, categories[i])));
+                    } else {
+                        System.out.println(displayFutureGenMessage(new ByteArrayOutputStream(), String.format(NegativeMessage, categories[i])));
+                    }
+                    getUserResponse(System.in, categoriesArray.get(i));
+                }
+            }
+        }
+
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
     public static String displayFutureGenMessage(ByteArrayOutputStream userDisplay, String stringthis) throws IOException {
-        byte [] introduction = stringthis.getBytes();
-        userDisplay.write(introduction);
+        byte [] info = stringthis.getBytes();
+        userDisplay.write(info);
         return userDisplay.toString();
     }
     public static String getUserResponse(InputStream input, ArrayList st){
@@ -42,5 +61,34 @@ public class FutureGen {
         st.add(inp);
         return String.valueOf(st.get(st.lastIndexOf(inp)));
     }
+    /*public static void populateArrayList(InputStream input) throws IOException {
+
+                getUserResponse(input, categoriesArray.get(i));
+    }*/
+        public static void addCategories(){
+        categoriesArray.add(personToMarry);
+        categoriesArray.add(PlaceToLive);
+        categoriesArray.add(typeOfCar);
+        categoriesArray.add(typeOfPet);
+        categoriesArray.add(typeOfResidence);
+        categoriesArray.add(numberOfChildren);
+    }
+
+    public ArrayList<ArrayList<String>> getCategoriesArray(){
+        return categoriesArray;
+    }
+    public int getRandomNumber (){
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(300);
+        return randomInt;
+    }
+   /* public String showResults(String [] s, int r){
+        for( int i =0; i<r; i++){
+            getUserResponse();
+        }
+
+            System.out.println();
+        return null;
+    }*/
 
 }
--- src/FutureGenTest.java
+++ src/FutureGenTest.java
@@ -17,12 +17,21 @@ public class FutureGenTest {
     private ByteArrayInputStream userAnswer;
     private FutureGen fut = new FutureGen();
     private String sts = "Test Input";
+    private static final String [] cat = new String []{"Person you will marry (ex: Chris, Channing etc.)", " Country you will live in (ex: Russia, Japan etc.)", "Type of car you will drive (ex: Subaru, Limo etc.)"," Pet you will have (ex: Dog, Tiger etc.)", "Residence you will live in (ex: House, Mansion etc.)", "Number of Children you will pop out (ex: 2, 11 etc.)" };
+    private ArrayList<ByteArrayInputStream> allUsersAnswers = new ArrayList<ByteArrayInputStream>();
+    private String[] userInput = new String[] {"Chris", "Elad", "Ryan", "Moscow", "Pittsburgh", "Chicago", "Subaru", "Limo", "Toyota", "Cat", "Fish", "House", "Box", "Hotel", "7", "2", "1"};
+    private static final String PositiveMessage = "Please enter %s that you do like.";
+    private static final String NegativeMessage = "Please enter %s that you do not like.";
+
 
     @Before
     public void init(){ //setting up all the tests
 
         System.setOut(new PrintStream(userDisplay));
         userAnswer = new ByteArrayInputStream("Test Input".getBytes());
+        for(int i = 0; i < userInput.length; i++){
+            allUsersAnswers.add(new ByteArrayInputStream(userInput[i].getBytes()));
+        }
         System.setIn(userAnswer);
     }
 
@@ -40,8 +49,47 @@ public class FutureGenTest {
         fut.getUserResponse(userAnswer, stAr);
         assertEquals("Test Input", stAr.get(stAr.lastIndexOf("Test Input")));
     }
+    @Test
+    public void checkAddCategoriesMethod(){
+        fut.addCategories();
+        fut.getCategoriesArray();
+        assertEquals(fut.getCategoriesArray().size(), 6);
+    }
+   /*@Test
+     public void testAbleToPopulateArrayList()throws IOException{ //testing to see if the arraylist is populating user input
+        fut.addCategories();
+        String fml = null;
+       ArrayList<ArrayList<String>> list = fut.getCategoriesArray();
+       for (int i = 0; i < cat.length; i++) {
+           for (int j = 0; j <= 2; j++) {
+               if (j == 0 || j == 1) {
+                   System.out.println(fut.displayFutureGenMessage(new ByteArrayOutputStream(), String.format(PositiveMessage, cat[i])));
+               } else {
+                   System.out.println(fut.displayFutureGenMessage(new ByteArrayOutputStream(), String.format(NegativeMessage, cat[i])));
+               }
+               fml= fut.getUserResponse(allUsersAnswers.get(i), list.get(i));
+           }
+               assertEquals(userInput[i], fml);
+        }
+    }*/
+    @Test
+    public void numberWasGeneratedInRange() {
+        int random;
+        int min =0;
+        int max = 300;
+        for (int i = 0; i < 300; i++) {
+            random = fut.getRandomNumber();
+            assert(random>=min && random<=max);
 
+        }
+    }
+    /*@Test
+    public void correctResultsAreShown(){
+        int rand = fut.getRandomNumber();
+        fut.showResults(userInput,rand);
 
+    }
+*/
 
     @After
     public void cleanUP(){
